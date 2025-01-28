f9c908797d9fbbbcff469382e9c4aaddd932a125
om07github <mahalleom794@gmail.com>
2025-01-28 12:11:14 +0530

    first commit

---
+++ src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default conf
+// but using the config object helps centralize configuration management, making the code cleaner and more maintainable.he config object is used to store these variables, ensuring they are properly cast to strings and organized in one place
